20c9360340be22af96d702758df88d7c092b319e
denewsland-debug <denewsland@gmail.com>
2026-03-08 19:45:40 +0530

    Create script new code

---
+++ script.py
@@ -0,0 +1,29 @@
+import feedparser
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.methods.posts import NewPost
+
+rss_urls = [
+"https://news.google.com/rss/search?q=india+finance",
+"https://news.google.com/rss/search?q=stock+market+india",
+"https://news.google.com/rss/search?q=technology+news+india"
+]
+
+wp = Client(
+"https://denewsland.in/xmlrpc.php",
+"YOUR_USERNAME",
+"APPLICATION_PASSWORD"
+)
+
+for url in rss_urls:
+
+    feed = feedparser.parse(url)
+
+    for entry in feed.entries[:3]:
+
+        post = WordPressPost()
+
+        post.title = entry.title
+        post.content = entry.summary
+        post.post_status = "publish"
+
+        wp.call(NewPost(post))
